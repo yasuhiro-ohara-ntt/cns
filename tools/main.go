4533caf63186c67dbeed4469173b13c08f500bd5
= <=>
2018-04-13 16:11:21 +0900

    2018.04.13-16:11

--- tools/main.go
+++ tools/main.go
@@ -2,10 +2,24 @@
 package main
 
 import (
-        "fmt"
+    "fmt"
+    "net"
+    // "runtime"
+    "github.com/vishvananda/netns"
 )
 
 func main() {
-        fmt.Println("slankdev")
-}
+    // runtime.LockOSThread()
+    // defer runtime.UnlockOSThread()
 
+    origns, _ := netns.Get()
+    defer origns.Close()
+
+    newns, _ := netns.New()
+    netns.Set(newns)
+    defer newns.Close()
+
+    ifaces, _ := net.Interfaces()
+    fmt.Printf("Interfaces: %v\n", ifaces)
+    netns.Set(origns)
+}
