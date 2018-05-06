7a783e898578f930f15741ef8357b6e80fc17525
Hiroki Shirokura <slank.dev@gmail.com>
2018-05-06 15:38:07 +0000

    2018.05.06-15:38

--- phase2/setup.sh
+++ phase2/setup.sh
@@ -21,21 +21,6 @@ lxc launch $image r9
 lxc launch $image client
 lxc launch $image server
 
-lxc exec r0     -- ip r del default
-lxc exec r1     -- ip r del default
-lxc exec r2     -- ip r del default
-lxc exec r3     -- ip r del default
-lxc exec r4     -- ip r del default
-lxc exec r5     -- ip r del default
-lxc exec r6     -- ip r del default
-lxc exec r7     -- ip r del default
-lxc exec r8     -- ip r del default
-lxc exec r9     -- ip r del default
-lxc exec client -- ip r del default
-lxc exec client -- ip r add default via 192.168.10.1
-lxc exec server -- ip r del default
-lxc exec server -- ip r add default via 192.168.20.2
-
 bin/lxc_attach_netns.sh r0 r0
 bin/lxc_attach_netns.sh r1 r1
 bin/lxc_attach_netns.sh r2 r2
@@ -65,6 +50,22 @@ $GOPATH/bin/koko -n r8,ifr1,192.168.130.1/24 -n r1,ifr8,192.168.130.2/24 # 192.1
 $GOPATH/bin/koko -n r9,ifr2,192.168.140.1/24 -n r2,ifr9,192.168.140.2/24 # 192.168.140.0/24
 $GOPATH/bin/koko -n r5,client,192.168.10.1/24 -n client,r5,192.168.10.2/24 # client
 $GOPATH/bin/koko -n server,r9,192.168.20.1/24 -n r9,server,192.168.20.2/24 # server
+
+lxc exec r0     -- ip r del default
+lxc exec r1     -- ip r del default
+lxc exec r2     -- ip r del default
+lxc exec r3     -- ip r del default
+lxc exec r4     -- ip r del default
+lxc exec r5     -- ip r del default
+lxc exec r6     -- ip r del default
+lxc exec r7     -- ip r del default
+lxc exec r8     -- ip r del default
+lxc exec r9     -- ip r del default
+lxc exec client -- ip r del default
+lxc exec server -- ip r del default
+
+lxc exec client -- ip r add default via 192.168.10.1
+lxc exec server -- ip r add default via 192.168.20.2
 
 ip netns delete r0
 ip netns delete r1
