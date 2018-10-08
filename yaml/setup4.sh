eea5c137a65764a73508459300e11bf12f121d41
Hiroki Shirokura <slankdev@nttv6.jp>
2018-10-08 08:46:37 +0000

    Del old files

--- yaml/setup4.sh
+++ yaml/setup4.sh
@@ -24,15 +24,15 @@ docker exec R1 bash -c "\
 	  -c 'interface net0' -c 'ip address 10.3.0.2/24' -c 'exit' \
 	  -c 'interface net1' -c 'ip address 10.1.0.1/24' -c 'exit' \
 		-c 'router bgp 200' \
-			-c 'bgp router-id 10.255.0.10' \
-			-c 'neighbor 10.1.0.2 remote-as 200' \
+			-c 'bgp router-id 10.255.0.11' \
+			-c 'neighbor 10.1.0.2 remote-as 300' \
 			-c 'neighbor 10.3.0.1 remote-as 100' "
 docker exec R2 bash -c "\
 	/usr/bin/vtysh -c 'conf t' \
 	  -c 'interface lo' -c 'ip address 10.255.0.10/32' -c 'exit' \
 	  -c 'interface net0' -c 'ip address 10.1.0.2/24' -c 'exit' \
 	  -c 'interface net1' -c 'ip address 192.168.100.1/24' -c 'exit' \
-		-c 'router bgp 100' \
+		-c 'router bgp 300' \
 			-c 'bgp router-id 10.255.0.12' \
 			-c 'neighbor 10.1.0.1 remote-as 200' \
 			-c 'network 192.168.100.0/24' "
